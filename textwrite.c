94e33f81117acdb9428fe5f10785afa486789b21
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-03-10 00:33:52 +0500

    main: spell checker called via exec

--- textwrite.c
+++ textwrite.c
@@ -7,61 +7,69 @@
 #include <sys/wait.h>
 #include <string.h>
 
+char* create_attach_shared_memory(int *shmid);
 
-int main() {
+int main(){
+    
     int shmid;
-    key_t key;
-    char *shm;
-    // Generate a unique key
-    key = ftok("/home/deviant/Data/University/5-OS/Assignments/1/textwrite.c", 'R');
-    // printf("Key: %d\n", key);
-    if (key == -1) {
-        perror("ftok");
-        exit(1);
-    }
-    // Create a shared memory segment
-    shmid = shmget(key, sizeof(char) * 100, IPC_CREAT | 0666);
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
-    }
-    // Attach the shared memory segment
-    shm = (char *)shmat(shmid, NULL, 0);
-    if (shm == (char *)-1) {
-        perror("shmat");
-        exit(1);
-    }
+    char *shm_ptr = create_attach_shared_memory(&shmid);
+
     // Write data to the shared memory segment
     char str[100];
     printf("\nEnter the string to be checked (characters only): ");
     fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = 0;
-    sprintf(shm, str);
+    sprintf(shm_ptr, str);
     
     // Fork a child process
     pid_t pid = fork();
-    if (pid == -1) {
+    if (pid == -1){
         perror("fork");
         exit(1);
     }
-    if (pid == 0) {
-        // Child process
-        printf("Child process: %s\n", shm);
-        exit(0);
-    } else {
+    if (pid == 0){
+        // Detach shared memory segment from address space
+        if (shmdt(shm_ptr) == -1){
+            perror("shmdt");
+            exit(1);
+        }
+        // Execute a new program
+        execl("spellchecker", NULL);
+        perror("execl");
+        exit(1);
+    }
+    else{
         // Parent process
         wait(NULL);
-        printf("Parent process: %s\n", shm);
+        printf("\nEntered string : %s\n", str);
+        printf("Spell errors : %s\n", shm_ptr);
         // Detach the shared memory segment
-        if (shmdt(shm) == -1) {
+        if (shmdt(shm_ptr) == -1){
             perror("shmdt");
             exit(1);
         }
         // Remove the shared memory segment
-        if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+        if (shmctl(shmid, IPC_RMID, NULL) == -1){
             perror("shmctl");
             exit(1);
         }
     }
     return 0;
+}
+
+char* create_attach_shared_memory(int *shmid){
+
+    key_t key=221669;
+    // Create a shared memory segment
+    *shmid = shmget(key, sizeof(char) * 100, IPC_CREAT | 0666);
+    if (*shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    // Attach the shared memory segment
+    char *shm_ptr = (char *)shmat(*shmid, NULL, 0);
+    if (shm_ptr == (char *)-1){
+        perror("shmat");
+        exit(1);
+    }
+    return shm_ptr;
 }
