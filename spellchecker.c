7dfd5759f8bd8b86749b1b6eddac85291c2476fc
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-03-09 17:36:28 +0500

    modified spell checker

--- spellchecker.c
+++ spellchecker.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/shm.h>
 
 #define MAX_WORD_LENGTH 100
 
+int loadDictionary(char *filename, char dictionary[][MAX_WORD_LENGTH]);
+void checkSpelling(char *shm_ptr, int *numWords, char dictionary[][MAX_WORD_LENGTH]);
+
 int main() {
+
+    // Read the dictionary file into memory
+    char dictionary[10000][MAX_WORD_LENGTH];
+    char fileName[]= "dictionary.txt";
+    int numWords = loadDictionary(fileName, dictionary);
+
+    key_t key=221669;
+    int shmid = shmget(key, sizeof(char) * 100, 0666);
+    if (shmid == -1){
+        perror("shmget");
+        exit(1);
+    }
+    char *shm_ptr = (char *)shmat(shmid, NULL, 0);
+    if (shm_ptr == (char *)-1){
+        perror("shmat");
+        exit(1);
+    }
+
+    checkSpelling(shm_ptr, &numWords, dictionary);
+    
+    return 0;
+}
+
+int loadDictionary(char *filename, char dictionary[][MAX_WORD_LENGTH]){
+
     // Open the dictionary file
     FILE *dictionaryFile = fopen("./dictionary.txt", "r");
     if (dictionaryFile == NULL) {
@@ -13,9 +42,8 @@ int main() {
     }
 
     // Read the dictionary file into memory
-    char dictionary[10000][MAX_WORD_LENGTH];
     int numWords = 0;
-    while (fgets(dictionary[numWords], MAX_WORD_LENGTH, dictionaryFile) != NULL) {
+    while (fgets(dictionary[numWords], MAX_WORD_LENGTH, dictionaryFile) != NULL){
         // Remove the newline character from each word
         dictionary[numWords][strcspn(dictionary[numWords], "\n")] = '\0';
         numWords++;
@@ -23,18 +51,22 @@ int main() {
 
     // Close the dictionary file
     fclose(dictionaryFile);
+    return numWords;
+}
 
-    // Get the string from the user
-    char inputString[MAX_WORD_LENGTH];
-    printf("Enter a string: ");
-    fgets(inputString, MAX_WORD_LENGTH, stdin);
+void checkSpelling(char *shm_ptr, int *numWords, char dictionary[][MAX_WORD_LENGTH]){
+
+    // Get string from shared memory
+    char inputString[100];
+    strcpy(inputString, shm_ptr);
+    // printf("\n\nFROM REPLACED CHILD: %s\n\n",inputString);
 
     // Split the string into individual words
     char *word = strtok(inputString, " \n");
     while (word != NULL) {
         // Check if the word is in the dictionary
         int found = 0;
-        for (int i = 0; i < numWords; i++) {
+        for (int i = 0; i < *numWords; i++) {
             if (strcmp(word, dictionary[i]) == 0) {
                 found = 1;
                 break;
@@ -43,15 +75,10 @@ int main() {
 
         // Print the result
         if (found){
-            printf("%s is spelled correctly.\n", word);
+            // printf("Sending to shared memory\n");
+            sprintf(shm_ptr, word);
         }
-        else{
-            printf("%s is misspelled.\n", word);
-        }
-
         // Get the next word
         word = strtok(NULL, " \n");
     }
-
-    return 0;
 }
